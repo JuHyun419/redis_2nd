c8e1869086a996afb3b0219208957141e9014f68
juhyun-leee <dev.juhyun@gmail.com>
2025-03-23 20:19:12 +0900

    test: 검색 기능 테스트 코드 추가 (제목/장르/캐싱 케이스)

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -3,4 +3,10 @@ dependencies {
 
     implementation(project(":domain"))
     implementation(project(":infrastructure"))
+
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.2")
+    testImplementation("io.kotest:kotest-framework-api:5.3.2")
+    testImplementation("io.mockk:mockk:1.13.5")
 }
