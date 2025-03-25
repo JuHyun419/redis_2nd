5c6119803b3316c7cada0b15a9ce8eb412b95d5d
juhyun-leee <dev.juhyun@gmail.com>
2025-03-25 22:42:56 +0900

    refactor: (2주차 리뷰 반영) movies 복합 인덱스 순서 변경

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -15,7 +15,7 @@ CREATE TABLE movies
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE INDEX idx_title_genre ON movies (title, genre);
+CREATE INDEX idx_genre_title ON movies (genre, title);
 
 
 -- 극장 테이블
