45c3fdb770a8f10fc887b89f549362dbca41f1f5
juhyun-leee <dev.juhyun@gmail.com>
2025-04-05 20:53:28 +0900

    feat: jacoco 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("io.spring.dependency-management") version "1.1.7"
+    jacoco
 }
 
 java {
@@ -27,11 +28,48 @@ subprojects {
         plugin("kotlin-kapt")
         plugin("io.spring.dependency-management")
         plugin("kotlin-spring")
+        apply(plugin = "jacoco")
     }
 
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+    }
+
+    jacoco {
+        toolVersion = "0.8.11"
+    }
+
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test)
+        reports {
+            html.required = true
+            xml.required = true
+            csv.required = false
+            xml.outputLocation = file("${layout.buildDirectory}/reports/jacoco.xml")
+        }
+
+        val qDomainList = mutableListOf<String>()
+        for (char in 'A'..'Z') {
+            qDomainList.add("**/Q$char*")
+        }
+
+        classDirectories.setFrom(
+            files(
+                classDirectories.files.map {
+                    fileTree(it) {
+                        exclude(
+                            qDomainList,
+                        )
+                    }
+                }
+            )
+        )
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        finalizedBy(tasks.jacocoTestReport)
     }
 
     tasks {
@@ -40,10 +78,6 @@ subprojects {
                 freeCompilerArgs = listOf("-Xjsr305=strict")
                 jvmTarget = "21"
             }
-        }
-
-        withType<Test> {
-            useJUnitPlatform()
         }
     }
 }
